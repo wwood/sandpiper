c748b025d82e4ddd6c6cb897c6de4b3ec946a55a
Ben Woodcroft <donttrustben@gmail.com>
2021-09-21 15:06:30 +1000

    possibly working sample metadata extractor.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 use std::fs::File;
 use flate2::read::GzDecoder;
 use tar::Archive;
-use minidom::Element;
 use std::io::Read;
-use anyhow::Result;
-use minidom::quick_xml::Reader;
+use xmltree::Element;
+use std::env;
 
 fn main() {
-    let path = "test/data/test_2_runs.tar.gz";
+    // let path = "test/data/test_2_runs.tar.gz";
 
-    metadata(path);
+    // metadata(path);
+    metadata(&env::args().collect::<Vec<_>>()[1])
 }
 
 fn metadata(tar_path: &str) {
@@ -20,12 +20,11 @@ fn metadata(tar_path: &str) {
         let mut e = entry.expect("tar gz parsing error");
         let xml_path = e.path().unwrap().to_str().unwrap().to_owned();
         if xml_path.ends_with(".sample.xml") {
-            println!("{}: {}", e.path().unwrap().to_str().unwrap(), e.size());
+            // println!("{}: {}", e.path().unwrap().to_str().unwrap(), e.size());
             let mut xml = String::new();
             e.read_to_string(&mut xml).expect("Failed to read XML string");
-            let mut quickxml_reader = Reader::from_str(&xml);
             // let root: Element = xml.parse().expect(&format!("Failed to parse {}",xml_path));
-            let root: Element = Element::from_reader(&mut quickxml_reader).expect(&format!("Failed to parse {}",xml_path));
+            let mut root: Element = Element::parse(xml.as_bytes()).expect(&format!("Failed to parse {}",xml_path));
 
             /* <?xml version="1.0" encoding="UTF-8"?>
               <SAMPLE_SET xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
@@ -59,10 +58,46 @@ fn metadata(tar_path: &str) {
 
             */
             // root.get_child("SAMPLE_NAME", namespace: NS)
-            println!("{}", xml_path);
-            // for child in root.children() {
-            //   child.
-            // }
+            let sample = root.get_mut_child("SAMPLE").expect(&format!("Cannot find SAMPLE in {}", xml_path));
+            let sample_attributes1 = sample.get_mut_child("SAMPLE_ATTRIBUTES");
+            if sample_attributes1.is_none() {
+              eprintln!("Cannot find SAMPLE_ATTRIBUTES in {}", xml_path);
+              continue;
+            }
+            let sample_attributes = sample_attributes1.unwrap();
+
+            // for child in sample_attributes.child_iter()
+            let run_identifier = xml_path.split("/").collect::<Vec<_>>()[0];
+            // println!("{}", run_identifier);
+            let mut printed_any = false;
+
+            while let Some(unw) = sample_attributes.take_child("SAMPLE_ATTRIBUTE") {
+
+              let tag1 = &unw.get_child("TAG");
+              if tag1.is_none() {continue}
+              let tag_children = &tag1.unwrap().children;
+              if tag_children.len() == 0 {
+                eprintln!("Bad tag_children: {:?}", tag_children);
+                continue;
+              }
+              let tag = tag_children[0].as_text().unwrap();
+
+              let value1 = &unw.get_child("VALUE");
+              if value1.is_none() {continue}
+              let value_children = &value1.unwrap().children;
+              if value_children.len() == 0 {
+                eprintln!("Bad value_children: {:?}", value_children);
+                continue;
+              }
+              let value = value_children[0].as_text().unwrap();
+
+              printed_any = true;
+              println!("{}\t{}\t{}", run_identifier, tag, value);
+            }
+
+            if !printed_any {
+              eprintln!("No metadata found for {}", run_identifier);
+            }
         }
     }
 }
