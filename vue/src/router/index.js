9335ae4b50ebeb393e4c5345cfc43e60fb3768a3
Ben Woodcroft <benjwoodcroft@gmail.com>
2022-01-13 18:14:23 +1000

    vue: Sunburst example code working.

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Search from '../views/Search.vue'
+import Run from '../views/Run.vue'
 
 Vue.use(VueRouter)
 
@@ -11,12 +13,14 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/search',
+    name: 'Search',
+    component: Search
+  },
+  {
+    path: '/run',
+    name: 'Run',
+    component: Run
   }
 ]
 
