befe4c5702085e307253b7b0fcd891cd5edfdb2e
Ben Woodcroft <benjwoodcroft@gmail.com>
2022-03-29 08:12:51 +1000

    add Oruga/bulma

--- vue3/src/main.ts
+++ vue3/src/main.ts
@@ -6,4 +6,8 @@ const app = createApp(App);
 
 app.use(router);
 
+import Oruga from '@oruga-ui/oruga-next'
+import { bulmaConfig } from '@oruga-ui/theme-bulma'
+app.use(Oruga, bulmaConfig)
+
 app.mount("#app");
