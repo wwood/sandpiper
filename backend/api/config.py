df9504f9a1c1b0fd7ab0e99dc12cb85bb93372bb
Ben Woodcroft <benjwoodcroft@gmail.com>
2022-05-15 06:51:01 +1000

    backend/config: Use file existence to switch.

--- backend/api/config.py
+++ backend/api/config.py
@@ -2,18 +2,25 @@
     config.py
     - settings for the flask application object
 """
+import os
 
-class BaseConfig(object):
+class BaseConfig:
     DEBUG = True
 
-    ## For local testing
-    # SQLALCHEMY_DATABASE_URI = 'sqlite:///6_runs.sandpiper.sqlite3'
+    LYRA_DB_PATH = 'sqlite:////data1/woodcrob/sra_20211215.mach1.sandpiper6.sqlite3'
 
-    ## For lyra testing
-    # SQLALCHEMY_DATABASE_URI = 'sqlite:////data1/woodcrob/sra_20211215.mach1.sandpiper6.sqlite3'
-    
-    ## For deployment
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///db/sra_20211215.mach1.sandpiper6.sqlite3'
+    if os.path.exists(os.path.join(os.path.dirname(__file__), 'running_on_lyra')):
+        SQLALCHEMY_DATABASE_URI = LYRA_DB_PATH
+
+    else:
+        ## For local testing
+        # SQLALCHEMY_DATABASE_URI = 'sqlite:///6_runs.sandpiper.sqlite3'
+
+        ## For lyra testing
+        # SQLALCHEMY_DATABASE_URI = LYRA_DB_PATH
+        
+        ## For deployment
+        SQLALCHEMY_DATABASE_URI = 'sqlite:///db/sra_20211215.mach1.sandpiper6.sqlite3'
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO=True
