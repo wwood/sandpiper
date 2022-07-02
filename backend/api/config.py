12e0284c44f448edaef9e63a7a25a5162bb5d409
Ben Woodcroft <benjwoodcroft@gmail.com>
2022-07-03 09:23:10 +1000

    backend: Fix prod db path.

--- backend/api/config.py
+++ backend/api/config.py
@@ -8,6 +8,7 @@ class BaseConfig:
     DEBUG = True
 
     LYRA_DB_PATH = 'sqlite:///db/sandpiper_16.sqlite3'
+    PROD_DB_PATH = LYRA_DB_PATH
 
     if os.path.exists(os.path.join(os.path.dirname(__file__), 'running_on_lyra')):
         SQLALCHEMY_DATABASE_URI = LYRA_DB_PATH
@@ -15,7 +16,7 @@ class BaseConfig:
 
     else:        
         ## For deployment
-        SQLALCHEMY_DATABASE_URI = 'sqlite:///db/sandpiper_15.sqlite3'
+        SQLALCHEMY_DATABASE_URI = PROD_DB_PATH
     print("Connecting to db {}".format(SQLALCHEMY_DATABASE_URI))
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
