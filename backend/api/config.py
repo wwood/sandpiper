65b6785007e247595446f3e9fe3cbb30c80ae44e
Ben Woodcroft <benjwoodcroft@gmail.com>
2025-04-17 08:17:40 +1000

    Fix db path.

--- backend/api/config.py
+++ backend/api/config.py
@@ -8,12 +8,11 @@ class BaseConfig:
     DEBUG = True
 
     # Recently, it because required somehow that the DB path is an absolute path
-    DB_NAME = 'sandpiper_31.sqlite3'
+    DB_NAME = 'sandpiper_33.sqlite3'
     # DB_NAME = 'sandpiper_18_test.sqlite3'
     LYRA_DB_PATH = 'sqlite:///'+os.path.join(os.path.dirname(__file__), '../db/{}'.format(DB_NAME))
-    LYRA_DB_PATH = 'sqlite:////scratch/sandpiper/sandpiper_33.sqlite3'
+    # LYRA_DB_PATH = 'sqlite:////scratch/sandpiper/sandpiper_33.sqlite3'
     # LYRA_DB_PATH = 'sqlite:////scratch/sandpiper/sandpiper_19_test.sqlite3'
-    # '+os.path.join(os.path.dirname(__file__), '../db/{}'.format(DB_NAME))
     PROD_DB_PATH = LYRA_DB_PATH #'sqlite:////data/{}'.format(DB_NAME)
 
     if os.path.exists(os.path.join(os.path.dirname(__file__), 'running_on_lyra')):
